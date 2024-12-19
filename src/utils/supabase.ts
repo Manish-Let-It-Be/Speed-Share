dc92ddaa69f28b8f45b2660fa84b8b69f5d5d00f
Manish <m4manishp4prasad@gmail.com>
2024-12-19 22:38:47 +0530

    New

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
 const SUPABASE_URL =process.env.SUPABASE_URL as string;
 const SUPABASE_ANON_KEY =process.env.SUPABASE_ANON_KEY as string;
