d77aff23e9091d1bf85511b227d46c95b697eb33
Manish <m4manishp4prasad@gmail.com>
2024-12-19 21:52:11 +0530

    new

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = process.env.SUPABASE_URL as string;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY as string;
 
-console.log('SUPABASE_URL:', SUPABASE_URL);
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
 console.log('SUPABASE_ANON_KEY:', SUPABASE_ANON_KEY);
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
